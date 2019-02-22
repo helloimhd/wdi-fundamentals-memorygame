047fcd908cdf8e5062796ea8f51c5f58e4f8eb1d
herda <herda58@gmail.com>
2019-02-22 16:08:27 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,19 +2,23 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+		alert("You found a match!");
 	} else if (cardsInPlay[0] !== cardsInPlay[1]) {
 		alert("Sorry, try again.");
 	}
-}
+};
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	// add the card user flipped to cardsInPlay 
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
+
+flipCard(0);	
+flipCard(1);
